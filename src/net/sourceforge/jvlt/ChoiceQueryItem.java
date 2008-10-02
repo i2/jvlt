f5abade7ba258ea06b0d64f9e50ebf874a89c4d0
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2008-10-02 18:19:05 +0000

    New query item for simple (non-array) choice attributes
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@85 2e8f9017-362f-4c5f-8fba-3cd18fd43268

---
+++ src/net/sourceforge/jvlt/ChoiceQueryItem.java
@@ -0,0 +1,24 @@
+package net.sourceforge.jvlt;
+
+public class ChoiceQueryItem extends ObjectQueryItem {
+	public static final int EQUALS = 0;
+	public static final int NOT_EQUAL = 1;
+	
+	public ChoiceQueryItem(String name, int type, Object value) {
+		super(name, type, value);
+	}
+	
+	public ChoiceQueryItem() { this("", EQUALS, null); }
+	
+	public boolean objectMatches(Object obj) {
+		if (obj==null || _value==null)
+			return obj==null && _value==null;
+		
+		if (_type == StringQueryItem.EQUALS)
+			return obj.equals(_value);
+		else if (_type == NOT_EQUAL)
+			return ! obj.equals(_value);
+		else
+			return false;
+	}
+}
