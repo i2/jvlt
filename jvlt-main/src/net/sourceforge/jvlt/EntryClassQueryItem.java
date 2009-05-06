52edfccf24e96e9fe032559c57ad1aa753346438
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-05-06 12:03:33 +0000

    Store entry class as string in order enable serialization
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@191 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/EntryClassQueryItem.java
+++ jvlt-main/src/net/sourceforge/jvlt/EntryClassQueryItem.java
@@ -4,28 +4,29 @@ public class EntryClassQueryItem extends ObjectQueryItem {
 	public static final int EQUALS = 0;
 	public static final int NOT_EQUAL = 1;
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param name The name
+	 * @param type The type
+	 * @param value The value (of type java.lang.String)
+	 */
 	public EntryClassQueryItem(String name, int type, Object value) {
-		super(name, type, value); }
+		super(name, type, value);
+	}
 		
 	public EntryClassQueryItem() { super(); }
 	
 	public boolean objectMatches(Object obj) {
+		EntryClass ec = (EntryClass) obj;
 		if (_type == EntryClassQueryItem.EQUALS)
-			return _value==null ? obj==null : _value.equals(obj);
+			return _value==null ? obj==null
+					: ec != null && _value.equals(ec.getName());
 		else if (_type == EntryClassQueryItem.NOT_EQUAL)
-			return ! (_value==null ? obj==null : _value.equals(obj));
+			return ! (_value==null ? obj==null
+					: ec != null && _value.equals(ec.getName()));
 		else
 			return false;
 	}
-
-	protected String convertToString(Object value) {
-		if (value == null)
-			return "";
-		else
-			return ((EntryClass) value).getName();
-	}
-		
-	protected Object createFromString(String value) {
-		return new EntryClass(value); }
 }
 
