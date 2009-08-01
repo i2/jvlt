b1eaf1b52edd0cd88c4f1174fe3db842f0848b7f
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-08-01 19:53:40 +0000

    Added static method selectSaveFile for selecting a file ensuring that it has the correct extension
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@236 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/DictFileChooser.java
+++ jvlt-main/src/net/sourceforge/jvlt/DictFileChooser.java
@@ -1,19 +1,35 @@
 package net.sourceforge.jvlt;
 
+import java.awt.Component;
 import java.io.File;
 import javax.swing.JFileChooser;
 
 public class DictFileChooser extends JFileChooser {
 	private static final long serialVersionUID = 1L;
 
-	public static final int JVLT_FILES = 0;
-	public static final int CSV_FILES = 1;
-	public static final int HTML_FILES = 2;
+	public enum FileType {
+		JVLT_FILES("jvlt", "jvlt_files"),
+		CSV_FILES("csv", "csv_files"),
+		HTML_FILES("html", "html_files");
+		
+		private String extension;
+		private String description;
+		
+		private FileType(String extension, String description) {
+			this.extension = extension;
+			this.description = description;
+		}
+		
+		public String getExtension() { return this.extension; }
+		
+		public String getDescription() { return this.description; }
+	}
 	
 	public DictFileChooser(String file_name) {
-		this(file_name, JVLT_FILES); }
+		this(file_name, FileType.JVLT_FILES);
+	}
 
-	public DictFileChooser(String file_name, int type) {
+	public DictFileChooser(String file_name, FileType type) {
 		if (file_name != null && ! file_name.equals("")) {
 			File file = new File(file_name);
 			File parent = file.getParentFile();
@@ -21,23 +37,27 @@ public class DictFileChooser extends JFileChooser {
 				setCurrentDirectory(parent);
 		}
 		
-		SimpleFileFilter filter = null;
-		if (type == JVLT_FILES) {
-			filter = new SimpleFileFilter(
-				GUIUtils.getString("Labels", "jvlt_files"));
-			filter.addExtension("jvlt");
-		} else if (type == CSV_FILES) {
-			filter = new SimpleFileFilter(
-				GUIUtils.getString("Labels", "csv_files"));
-			filter.addExtension("csv");
-		} else if (type == HTML_FILES) {
-			filter = new SimpleFileFilter(
-				GUIUtils.getString("Labels", "html_files"));
-			filter.addExtension("html");
-		}
+		SimpleFileFilter filter = new SimpleFileFilter(
+				GUIUtils.getString("Labels", type.getDescription()));
+		filter.addExtension(type.getExtension());
+		setFileFilter(filter);
+	}
+	
+	public static String selectSaveFile(String file_name, FileType type,
+			Component parent) {
+		DictFileChooser chooser = new DictFileChooser(file_name, type);
+		if (chooser.showSaveDialog(parent) != JFileChooser.APPROVE_OPTION)
+			return null;
 		
-		if (filter != null)
-			setFileFilter(filter);
+		String selected_file = chooser.getSelectedFile().getPath();
+		
+		/* Add extension if necessary */
+		String extension = type.getExtension();
+		if (selected_file.length() < extension.length() + 1
+				|| ! selected_file.toLowerCase().endsWith("." + extension))
+			selected_file = selected_file + "." + extension;
+		
+		return selected_file;
 	}
 }
 
