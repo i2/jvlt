1d9ca084cd4744f6e70cac63bf84a1dd354bdf17
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-08-05 19:18:34 +0000

    private -> protected
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@246 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/MessageDialog.java
+++ jvlt-main/src/net/sourceforge/jvlt/MessageDialog.java
@@ -15,24 +15,25 @@ class MessageDialog extends JDialog implements ActionListener {
 	public static final int OK_OPTION=4;
 	public static final int CANCEL_OPTION=8;
 	public static final int OK_CANCEL_OPTION=OK_OPTION | CANCEL_OPTION;
+	public static final int USER_OPTION=16;
 	
-	private static MessageDialog _dialog = null;
+	protected static MessageDialog _dialog = null;
 	
-	private boolean _show_details = false;
-	private int _options = OK_OPTION;
-	private int _result = OK_OPTION;
+	protected boolean _show_details = false;
+	protected int _options = OK_OPTION;
+	protected int _result = OK_OPTION;
 	
-	private Action _details_action;
-	private JButton _ok_button;
-	private JButton _cancel_button;
-	private JButton _details_button;
-	private JLabel _message_label;
-	private JScrollPane _details_scrpane;
-	private JTextArea _details_area;
-	private JLabel _icon_label;
-	private JPanel _button_panel;
-	private JPanel _content_pane;
-	private String _details;
+	protected Action _details_action;
+	protected JButton _ok_button;
+	protected JButton _cancel_button;
+	protected JButton _details_button;
+	protected JLabel _message_label;
+	protected JScrollPane _details_scrpane;
+	protected JTextArea _details_area;
+	protected JLabel _icon_label;
+	protected JPanel _button_panel;
+	protected JPanel _content_pane;
+	protected String _details;
 	
 	public MessageDialog(Frame parent, int type, int options,
 		String message, String details) {
@@ -101,6 +102,59 @@ class MessageDialog extends JDialog implements ActionListener {
 		}
 	}
 	
+	protected void initUI() {
+		Action ok_action = GUIUtils.createTextAction(this, "ok");
+		Action cancel_action = GUIUtils.createTextAction(this, "cancel");
+		_details_action = GUIUtils.createAnonymousAction(this, "details");
+		_message_label = new JLabel();
+		_message_label.setBorder(new EmptyBorder(5,5,5,5));
+		_details_area = new JTextArea();
+		_details_area.setEditable(false);
+		_details_area.setOpaque(false);
+		_details_scrpane = new JScrollPane(_details_area);
+		_details_scrpane.setPreferredSize(new Dimension(300,100));
+		_details_scrpane.setBorder(new TitledBorder(new EtchedBorder(
+			EtchedBorder.LOWERED), GUIUtils.getString("Labels", "details")));
+		_icon_label = new JLabel();
+		_details_button = new JButton(_details_action);
+		_ok_button = new JButton(ok_action);
+		_cancel_button = new JButton(cancel_action);
+		_button_panel = new JPanel();
+		_button_panel.setLayout(new GridBagLayout());
+		CustomConstraints cc = new CustomConstraints();
+		cc.update(0, 0, 1.0, 0.0);
+		_button_panel.add(Box.createHorizontalGlue(), cc);
+		
+		_content_pane = new JPanel();
+		_content_pane.setLayout(new GridBagLayout());
+		cc.update(0, 0, 0.0, 1.0);
+		_content_pane.add(_icon_label, cc);
+		cc.update(1, 0, 1.0, 1.0);
+		_content_pane.add(_message_label, cc);
+		_content_pane.add(_details_scrpane);
+		setContentPane(_content_pane);
+	}
+	
+	protected void updateButtonRow() {
+		CustomConstraints cc = new CustomConstraints();
+		_button_panel.remove(_ok_button);
+		_button_panel.remove(_cancel_button);
+		_button_panel.remove(_details_button);
+		int x=1;
+		if ((_options & OK_OPTION) != 0) {
+			cc.update(x++, 0, 0.0, 0.0);
+			_button_panel.add(_ok_button, cc);
+		}
+		if ((_options & CANCEL_OPTION) != 0) {
+			cc.update(x++, 0, 0.0, 0.0);
+			_button_panel.add(_cancel_button, cc);
+		}
+		if (_details != null && ! _details.equals("")) {
+			cc.update(x++, 0, 0.0, 0.0);
+			_button_panel.add(_details_button, cc);
+		}
+	}
+	
 	private void setMessages(String message, String details) {
 		_details = details;
 		_message_label.setText(message);
@@ -136,39 +190,6 @@ class MessageDialog extends JDialog implements ActionListener {
 		update();
 	}
 	
-	private void initUI() {
-		Action ok_action = GUIUtils.createTextAction(this, "ok");
-		Action cancel_action = GUIUtils.createTextAction(this, "cancel");
-		_details_action = GUIUtils.createAnonymousAction(this, "details");
-		_message_label = new JLabel();
-		_message_label.setBorder(new EmptyBorder(5,5,5,5));
-		_details_area = new JTextArea();
-		_details_area.setEditable(false);
-		_details_area.setOpaque(false);
-		_details_scrpane = new JScrollPane(_details_area);
-		_details_scrpane.setPreferredSize(new Dimension(300,100));
-		_details_scrpane.setBorder(new TitledBorder(new EtchedBorder(
-			EtchedBorder.LOWERED), GUIUtils.getString("Labels", "details")));
-		_icon_label = new JLabel();
-		_details_button = new JButton(_details_action);
-		_ok_button = new JButton(ok_action);
-		_cancel_button = new JButton(cancel_action);
-		_button_panel = new JPanel();
-		_button_panel.setLayout(new GridBagLayout());
-		CustomConstraints cc = new CustomConstraints();
-		cc.update(0, 0, 1.0, 0.0);
-		_button_panel.add(Box.createHorizontalGlue(), cc);
-		
-		_content_pane = new JPanel();
-		_content_pane.setLayout(new GridBagLayout());
-		cc.update(0, 0, 0.0, 1.0);
-		_content_pane.add(_icon_label, cc);
-		cc.update(1, 0, 1.0, 1.0);
-		_content_pane.add(_message_label, cc);
-		_content_pane.add(_details_scrpane);
-		setContentPane(_content_pane);
-	}
-	
 	private void update() {
 		String text;
 		if (_show_details)
@@ -182,24 +203,9 @@ class MessageDialog extends JDialog implements ActionListener {
 		}
 		_details_action.putValue(Action.NAME, text);
 		
-		CustomConstraints cc = new CustomConstraints();
-		_button_panel.remove(_ok_button);
-		_button_panel.remove(_cancel_button);
-		_button_panel.remove(_details_button);
-		int x=1;
-		if ((_options & OK_OPTION) != 0) {
-			cc.update(x++, 0, 0.0, 0.0);
-			_button_panel.add(_ok_button, cc);
-		}
-		if ((_options & CANCEL_OPTION) != 0) {
-			cc.update(x++, 0, 0.0, 0.0);
-			_button_panel.add(_cancel_button, cc);
-		}
-		if (_details != null && ! _details.equals("")) {
-			cc.update(x++, 0, 0.0, 0.0);
-			_button_panel.add(_details_button, cc);
-		}
+		updateButtonRow();
 		
+		CustomConstraints cc = new CustomConstraints();
 		_content_pane.remove(_details_scrpane);
 		_content_pane.remove(_button_panel);
 		int y = 1;
