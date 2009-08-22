cfa5b7ca034729a3ced4653b9c047762f176fbfb
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-08-22 19:45:08 +0000

    Added timestamp, notify listeners when new error output arrives
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@262 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/ErrorLog.java
+++ jvlt-main/src/net/sourceforge/jvlt/ErrorLog.java
@@ -6,22 +6,31 @@ import java.io.InputStreamReader;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class ErrorLog {
 	public static final int NUM_LINES = 1024;
 	
-	private static ErrorLog _log;
+	private static Date _date = new Date();
+	private static ErrorLog _log = new ErrorLog();
 	
 	public static void init() {
-		_log = new ErrorLog();
 		System.setErr(new PrintStream(_log._pos));
 	}
 	
 	public static ErrorLog getInstance() { return _log; }
 	
 	private LinkedList<String> _lines = new LinkedList<String>();
+	
+	private List<ChangeListener> _listeners = new ArrayList<ChangeListener>();
 	
 	private final PipedOutputStream _pos = new PipedOutputStream();
 	
@@ -36,10 +45,22 @@ public class ErrorLog {
 					try {
 						while (true) {
 							String line = reader.readLine();
+							line = line.replaceAll("\t", "    ");
+							_date.setTime(System.currentTimeMillis());
+							String date_string =
+								DateFormat.getTimeInstance().format(_date);
+							System.out.println(date_string + " " + line);
+							String line_with_date = date_string + " " + line + "\n";
 							synchronized (_lines) {
-								_lines.add(line);
+								_lines.add(line_with_date);
 								if (_lines.size() > NUM_LINES)
 									_lines.remove();
+							}
+							
+							// Notify listeners
+							synchronized(_listeners) {
+								for(ChangeListener l: _listeners)
+									l.stateChanged(new ChangeEvent(ErrorLog.this));
 							}
 						}
 					}
@@ -55,9 +76,17 @@ public class ErrorLog {
 		StringBuffer buffer = new StringBuffer();
 		synchronized (_lines) {
 			for (Iterator<String> it = _lines.iterator(); it.hasNext(); )
-				buffer.append(it.next() + "\n");
+				buffer.append(it.next());
 		}
 		
 		return buffer.toString();
+	}
+	
+	public void addChangeListener(ChangeListener listener) {
+		synchronized(_listeners) {
+			_listeners.add(listener);
+		}
+		
+		listener.stateChanged(new ChangeEvent(this));
 	}
 }
