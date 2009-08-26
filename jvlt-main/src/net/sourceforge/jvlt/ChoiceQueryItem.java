2b8eb0e0e50eda8c3eaa7b8628d1b8e9c8bd397a
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-08-26 07:44:47 +0000

    - Added checks for (non-)emptiness - Fixed checks for (non-)equality and substrings
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@277 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/ChoiceQueryItem.java
+++ jvlt-main/src/net/sourceforge/jvlt/ChoiceQueryItem.java
@@ -4,6 +4,8 @@ public class ChoiceQueryItem extends ObjectQueryItem {
 	public static final int EQUALS = 0;
 	public static final int NOT_EQUAL = 1;
 	public static final int CONTAINS = 2;
+	public static final int EMPTY = 3;
+	public static final int NOT_EMPTY = 4;
 	
 	public ChoiceQueryItem(String name, int type, Object value) {
 		super(name, type, value);
@@ -12,17 +14,34 @@ public class ChoiceQueryItem extends ObjectQueryItem {
 	public ChoiceQueryItem() { this("", EQUALS, null); }
 	
 	public boolean objectMatches(Object obj) {
-		if (obj==null || _value==null)
-			return obj==null && _value==null;
-		
-		if (_type == StringQueryItem.EQUALS)
-			return obj.toString().equals(_value.toString());
-		else if (_type == NOT_EQUAL)
-			return ! obj.toString().equals(_value.toString());
-		else if (_type == CONTAINS)
-			return obj.toString().toLowerCase().indexOf(
-					_value.toString().toLowerCase()) >= 0;
-		else
+		if (_type == StringQueryItem.EQUALS) {
+			if (obj == null)
+				return _value == null;
+			else if (_value == null)
+				return false;
+			else
+				return obj.toString().equals(_value.toString());
+		} else if (_type == NOT_EQUAL) {
+			if (obj == null)
+				return _value != null;
+			else if (_value == null)
+				return true;
+			else
+				return ! obj.toString().equals(_value.toString());
+		} else if (_type == CONTAINS) {
+			if (_value == null)
+				return true;
+			else if (obj == null)
+				return false;
+			else
+				return obj.toString().toLowerCase().indexOf(
+						_value.toString().toLowerCase()) >= 0;
+		} else if (_type == EMPTY) {
+			return obj == null || obj.toString().length() == 0;
+		} else if (_type == NOT_EMPTY) {
+			return obj != null && obj.toString().length() > 0;
+		} else {
 			return false;
+		}
 	}
 }
