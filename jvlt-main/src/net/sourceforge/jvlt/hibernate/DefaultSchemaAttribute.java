e375fd66c487fc9c7fe57ac5bae21cd2ceef51fd
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-10-17 08:02:14 +0000

    Removed hibernate package (will be replaced by persistence package soon)
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@355 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/hibernate/DefaultSchemaAttribute.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.jvlt.hibernate;
-
-public class DefaultSchemaAttribute extends SchemaAttribute {
-	private String value;
-	
-	public String getValue() { return this.value; }
-	
-	public void setValue(String v) { this.value = v; }
-}
