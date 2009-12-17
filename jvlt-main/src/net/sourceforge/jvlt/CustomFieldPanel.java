0844ac4fb1f704a9f17d05f8106de9c4be7988ba
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2009-12-17 19:57:53 +0000

    - Allow reordering of rows - Always double click to edit values
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@424 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/CustomFieldPanel.java
+++ jvlt-main/src/net/sourceforge/jvlt/CustomFieldPanel.java
@@ -9,9 +9,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EventObject;
 import java.util.List;
 
+import javax.swing.Action;
+import javax.swing.Box;
 import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,6 +23,8 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 
@@ -37,17 +43,9 @@ public class CustomFieldPanel extends JPanel {
 				boolean hasFocus, int row, int column) {
 			JLabel label = (JLabel) super.getTableCellRendererComponent(
 					table, value, isSelected, hasFocus, row, column);
-			if (value == null) {
-				if (column == 0) {
-					if (value == null)
-						label.setText(
-								GUIUtils.getString("Labels", "enter_name"));
-				} else if (column == 1) {
-					if (value == null)
-						label.setText(
-								GUIUtils.getString("Labels", "enter_value"));
-				}
-			}
+			if (value == null)
+				label.setText(GUIUtils.getString(
+						"Labels", "double_click_to_edit"));
 			
 			return label;
 		}
@@ -127,38 +125,75 @@ public class CustomFieldPanel extends JPanel {
 			values.remove(row);
 			fireTableRowsDeleted(row, row);
 		}
+		
+		public void insertRow(int row, String key, String value) {
+			if (row < 0 || row > keys.size())
+				return;
+			
+			keys.add(row, key);
+			values.add(row, value);
+			fireTableRowsInserted(row, row);
+		}
 	}
 	
-	private CustomFieldTableModel table_model;
+	private static class CustomFieldCellEditor extends DefaultCellEditor {
+		private static final long serialVersionUID = 1L;
+		
+		public CustomFieldCellEditor(JComboBox box) { super(box); }
+		
+		public CustomFieldCellEditor(JTextField field) { super(field); }
+		
+		@Override
+		public boolean isCellEditable(EventObject evt) {
+			return ((MouseEvent)evt).getClickCount() >= 2;
+		}
+	}
+	
+	private CustomFieldTableModel tableModel;
 	private int popupRow = -1; // Row on which popup menu was opened
 	
 	private JTable table;
 	private JComboBox keyBox;
 	private JTextField valueField;
-	private DefaultCellEditor keyCellEditor;
-	private DefaultCellEditor valueCellEditor;
+	private CustomFieldCellEditor keyCellEditor;
+	private CustomFieldCellEditor valueCellEditor;
 	private JPopupMenu menu;
+	private Action upAction;
+	private Action downAction;
 	
 	public CustomFieldPanel() {
+		CustomConstraints cc = new CustomConstraints();
+		
+		ActionListener upDownListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getActionCommand().equals("up"))
+					moveElement(table.getSelectedRow(), true);
+				else if (e.getActionCommand().equals("down"))
+					moveElement(table.getSelectedRow(), false);
+			}
+		};
+		upAction = GUIUtils.createIconAction(upDownListener, "up");
+		downAction = GUIUtils.createIconAction(upDownListener, "down");
+		
 		keyBox = new JComboBox();
 		keyBox.setEditable(true);
 		
 		valueField = new JTextField();
 		
-		table_model = new CustomFieldTableModel();
+		tableModel = new CustomFieldTableModel();
 		
 		ActionListener remove_listener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				table_model.removeRow(popupRow);
+				removeRow(popupRow);
 			}
 		};
 		menu = new JPopupMenu();
 		menu.add(GUIUtils.createTextAction(remove_listener, "remove"));
 		
-		keyCellEditor = new DefaultCellEditor(keyBox);
-		valueCellEditor = new DefaultCellEditor(valueField);
+		keyCellEditor = new CustomFieldCellEditor(keyBox);
+		valueCellEditor = new CustomFieldCellEditor(valueField);
 		
-		table = new JTable(table_model);
+		table = new JTable(tableModel);
 		table.getColumnModel().getColumn(0).setCellEditor(keyCellEditor);
 		table.getColumnModel().getColumn(1).setCellEditor(valueCellEditor);
 		table.getColumnModel().getColumn(0).setCellRenderer(
@@ -172,13 +207,33 @@ public class CustomFieldPanel extends JPanel {
 			public void mouseReleased(MouseEvent e) { maybeShowPopup(e); }
 		});
 		
+		ListSelectionListener selectionListener = new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				if (! e.getValueIsAdjusting())
+					updateActions();
+			}
+		};
+		table.getSelectionModel().addListSelectionListener(selectionListener);
+		
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setPreferredSize(new Dimension(400, 200));
 		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridBagLayout());
+		cc.update(0, 0, 1.0, 0.0);
+		buttonPanel.add(new JButton(upAction), cc);
+		cc.update(0, 1, 1.0, 0.0);
+		buttonPanel.add(new JButton(downAction), cc);
+		cc.update(0, 2, 1.0, 1.0);
+		buttonPanel.add(Box.createVerticalGlue(), cc);
+		
 		setLayout(new GridBagLayout());
-		CustomConstraints cc = new CustomConstraints();
 		cc.update(0, 0, 1.0, 1.0);
 		add(scrollPane, cc);
+		cc.update(1, 0, 0.0, 1.0);
+		add(buttonPanel, cc);
+		
+		updateActions();
 	}
 	
 	public void setChoices(Object[] choices) {
@@ -189,12 +244,12 @@ public class CustomFieldPanel extends JPanel {
 	
 	public StringPair[] getKeyValuePairs() {
 		ArrayList<StringPair> valueList = new ArrayList<StringPair>();
-		for (int i=0; i<table_model.keys.size(); i++) {
-			if (table_model.keys.get(i) != null
-					&& ! table_model.keys.get(i).equals("")
-					&& table_model.values.get(i) != null)
-				valueList.add(new StringPair(table_model.keys.get(i),
-						table_model.values.get(i)));
+		for (int i=0; i<tableModel.keys.size(); i++) {
+			if (tableModel.keys.get(i) != null
+					&& ! tableModel.keys.get(i).equals("")
+					&& tableModel.values.get(i) != null)
+				valueList.add(new StringPair(tableModel.keys.get(i),
+						tableModel.values.get(i)));
 		}
 		
 		return valueList.toArray(new StringPair[0]);
@@ -205,8 +260,8 @@ public class CustomFieldPanel extends JPanel {
 			return;
 		
 		for (StringPair p: pairs) {
-			table_model.keys.add(p.getFirst());
-			table_model.values.add(p.getSecond());
+			tableModel.keys.add(p.getFirst());
+			tableModel.values.add(p.getSecond());
 		}
 	}
 	
@@ -219,8 +274,35 @@ public class CustomFieldPanel extends JPanel {
 	private void maybeShowPopup(MouseEvent e) {
 		if (e.isPopupTrigger()) {
 			popupRow = table.rowAtPoint(e.getPoint());
-			if (table_model.getValueAt(popupRow, 0) != null)
+			if (tableModel.getValueAt(popupRow, 0) != null)
 				menu.show(e.getComponent(), e.getX(), e.getY());
 		}
+	}
+	
+	private void moveElement(int row, boolean up) {
+		String key = (String) tableModel.getValueAt(row, 0);
+		String value = (String) tableModel.getValueAt(row, 1);
+		
+		removeRow(row);
+		
+		int newRow = up ? row - 1 : row + 1;
+		tableModel.insertRow(newRow, key, value);
+		
+		table.getSelectionModel().setSelectionInterval(newRow, newRow);
+	}
+	
+	private void updateActions() {
+		int index = table.getSelectedRow();
+		upAction.setEnabled(index > 0
+				&& tableModel.getValueAt(index, 0) != null);
+		downAction.setEnabled(tableModel.getValueAt(index, 0) != null
+				&& tableModel.getValueAt(index+1, 0) != null);
+	}
+	
+	private void removeRow(int index) {
+		// Discard entered data
+		keyCellEditor.cancelCellEditing();
+		valueCellEditor.cancelCellEditing();
+		tableModel.removeRow(index);
 	}
 }
