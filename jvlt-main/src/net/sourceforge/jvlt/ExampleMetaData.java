2c1d8bf2df2a26df3fde1e84fba8acffe95cc990
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2010-01-05 07:50:47 +0000

    Re-added ID attribute (needed for "Edit" link in vocab tab)
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@438 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/ExampleMetaData.java
+++ jvlt-main/src/net/sourceforge/jvlt/ExampleMetaData.java
@@ -71,8 +71,6 @@ public class ExampleMetaData extends MetaData {
 		
 		addAttribute(new TextFragmentsAttribute());
 		addAttribute(new TranslationAttribute());
-		
-		removeAttribute("ID");
 	}
 }
 
