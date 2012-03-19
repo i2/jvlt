db1279644b4e940c2dcaacf4ce756f27dc1191d7
burchardt <burchardt@2e8f9017-362f-4c5f-8fba-3cd18fd43268>
2012-03-19 20:54:05 +0000

    Handle too large images (patch by Maithreyi Venkatesh - slightly modified)
    git-svn-id: https://svn.code.sf.net/p/jvlt/code/trunk@608 2e8f9017-362f-4c5f-8fba-3cd18fd43268

--- jvlt-main/src/net/sourceforge/jvlt/multimedia/ImageFile.java
+++ jvlt-main/src/net/sourceforge/jvlt/multimedia/ImageFile.java
@@ -1,6 +1,7 @@
 package net.sourceforge.jvlt.multimedia;
 
 import java.awt.Frame;
+import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,8 +13,9 @@ import net.sourceforge.jvlt.ui.utils.GUIUtils;
 import net.sourceforge.jvlt.utils.I18nService;
 
 /**
- * This class represents an image file that can be viewed with jVLT. Other image
- * files are represented by CustomMultimediaFile.
+ * This class represents an image file that can be viewed with jVLT. 
+ * Other image files are represented by CustomMultimediaFile.
+ * @author Maithu
  */
 public class ImageFile extends MultimediaFile {
 	public ImageFile(String file_name) {
@@ -24,6 +26,11 @@ public class ImageFile extends MultimediaFile {
 		this("");
 	}
 
+	/**
+	 * This deals with allowing the user to view the image read into jVLT.
+	 * @param parent jVLT Window
+	 * @throws IOException if any of the checks fail
+	 */
 	public void show(Frame parent) throws IOException {
 		File f = getFile();
 		if (!f.exists() || !f.isFile()) {
@@ -33,10 +40,25 @@ public class ImageFile extends MultimediaFile {
 		}
 
 		ImageIcon icon = new ImageIcon(f.getAbsolutePath());
+		if (isImageTooLarge(icon, parent.getBounds())) {
+			String msg = "Image is too large. Please choose an image smaller than the size of the jVLT window or enlarge the jVLT window to view image.";
+			throw new IOException(msg);
+		}
 		JDialog dlg = new JDialog(parent,
 				I18nService.getString("Labels", "image"), false);
 		JLabel lbl = new JLabel(icon);
 		dlg.setContentPane(lbl);
 		GUIUtils.showDialog(parent, dlg);
+	}
+
+	/**
+	 * Checks if dimensions of image file are larger than the dimensions of the jVLT window.
+	 * @param icon The icon to be checked
+	 * @param bounds The bounds of the jVLT window
+	 * @return true if image is larger than size of jVLT window, else returns false
+	 */
+	private boolean isImageTooLarge(ImageIcon icon, Rectangle bounds){
+		return (icon.getIconHeight() > bounds.getHeight()
+				|| icon.getIconWidth() > bounds.getWidth());
 	}
 }
